9f614f766b7d56546e4e92295fcbddd5eacf1bcc
Jonas Henrique <programeicafe@outlook.com>
2025-02-24 10:20:04 -0300

    feat: add route GET addresses consulted

---
+++ consulte-cep/Controllers/AddressesController.cs
@@ -0,0 +1,26 @@
+﻿using consulte_cep.Data;
+using consulte_cep.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace consulte_cep.Controllers
+{
+    [Route("/api/enderecos")]
+    [ApiController]
+    public class AddressesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AddressesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/enderecos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AddressConsulted>>> GetAddressesConsulted()
+        {
+            return await _context.Addresses.ToListAsync();
+        }
+    }
+}
